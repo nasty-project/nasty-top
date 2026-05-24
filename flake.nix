dc95dc0576a092f602958230a8ec6732f16c24e2
qubitnano <146656568+qubitnano@users.noreply.github.com>
2026-05-24 15:03:28 -0400

    remove useFetchCargoVendor
    warning: buildRustPackage: `useFetchCargoVendor` is non‐optional and
    enabled by default as of 25.05, remove it

--- flake.nix
+++ flake.nix
@@ -24,7 +24,6 @@
             pname = "nasty-top";
             version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
             src = ./.;
-            useFetchCargoVendor = true;
             cargoHash = "sha256-d1gVWeQMLx03/qE62qY4Yk+Xqa/bWfdpIg8sjV4XX50=";
             meta = {
               description = "A top-like TUI for bcachefs filesystems";
